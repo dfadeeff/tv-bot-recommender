3a49107e42a3f5dcf02b2ae22e9ba6019c432df7
Dmitry Fadeev <fadejeff@gmail.com>
2025-03-30 15:09:34 +0200

    simplified and getting no deployment yet

--- api/index.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.main import app as fastapi_app
-
-# This is the handler that Vercel looks for
-def handler(request):
-    return fastapi_app(request)
-
-# Vercel also looks for an "app" variable
-app = fastapi_app
